fab8d58f543f6e93ff821dd97d5de462b58e2458
Bigyan Paudel <bigyanpaudel0@gmail.com>
2020-05-26 14:38:09 +0545

    Initial commit

---
+++ calc.js
@@ -0,0 +1,19 @@
+var num1 = document.getElementById("num1");
+var num2 = document.getElementById("num2");
+var form = document.getElementById("xIsWhatPercentOfY");
+
+form.addEventListener('submit' ,function(event){
+	if(!num1.value || !num2.value){
+		alert("The fields cannot be empty");
+	}else{
+		var x = parseFloat(num1.value);
+		var y = parseFloat(num2.value);
+
+		var result = (x/y) * 100;
+		console.log(result);
+		event.preventDefault();
+
+		resultField.innerText = "The answer is:  " + result + "  	 %";
+	}
+
+} )
